4e774d78518daba46556a67f0f0d9ff5bb4ae12f
Michael Berger <michael.goetz.berger@telekom.de>
2020-01-07 10:21:37 +0100

    Create initial Spring-Boot Project

---
+++ src/main/java/com/wildcodeschool/skillhub/SkillHubApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.skillhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillHubApplication.class, args);
+	}
+
+}
