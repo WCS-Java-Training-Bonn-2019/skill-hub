10fac92fbf9622c383699bffebbccdf019975c9f
Michael Berger <michael.goetz.berger@telekom.de>
2020-01-16 14:28:15 +0100

    Remove unused import from 'UserController'

--- src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.wildcodeschool.skillhub.model.Skill;
-import com.wildcodeschool.skillhub.model.User;
 import com.wildcodeschool.skillhub.repository.SkillRepository;
 import com.wildcodeschool.skillhub.repository.UserRepository;
 
