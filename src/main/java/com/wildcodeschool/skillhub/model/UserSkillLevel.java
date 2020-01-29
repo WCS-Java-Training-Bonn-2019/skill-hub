acc64742b67991491b1f0092dceb91306655c1c3
Martin Langenbacher <martin.langenbacher@googlemail.com>
2020-01-29 13:41:40 +0100

    Draft: UserSkillLevel

--- src/main/java/com/wildcodeschool/skillhub/model/UserSkillLevel.java
+++ src/main/java/com/wildcodeschool/skillhub/model/UserSkillLevel.java
@@ -2,5 +2,33 @@ package com.wildcodeschool.skillhub.model;
 
 public class UserSkillLevel {
 	boolean hasSkill;
+	int userSkillLevelInt; 
+	
+	// constructor	
+	public UserSkillLevel() {
+		super();
+		this.hasSkill = false;
+		this.userSkillLevelInt = 1; // 1: basic / 2: advanced / 3: professional
+		
+	
+	// getter & setter	
+		
+		
+	}
 
+	public boolean isHasSkill() {
+		return hasSkill;
+	}
+
+	public void setHasSkill(boolean hasSkill) {
+		this.hasSkill = hasSkill;
+	}
+
+	public int getUserSkillLevelInt() {
+		return userSkillLevelInt;
+	}
+
+	public void setUserSkillLevelInt(int userSkillLevelInt) {
+		this.userSkillLevelInt = userSkillLevelInt;
+	}
 }
