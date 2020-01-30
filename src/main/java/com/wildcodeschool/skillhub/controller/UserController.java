a1b7378ccb787b0643e10cdd13cc53dcccaace30
Stephanie Bauer <stephanie.bauer216@gmx.de>
2020-01-30 13:48:10 +0100

    working on Post method upsert

--- src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
 	// TODO For testing only
 	// Show edit user form
 	@GetMapping("/user/edit")
-	public String showEditUserForm(@ModelAttribute UserForm userForm, @RequestParam(required = false) Long id) {
+	public String showEditUserForm(UserForm userForm, @RequestParam(required = false) Long id) {
 
 		User user = new User();
 
@@ -77,7 +77,8 @@ public class UserController {
 		UserSkillLevel userSkillLevel;
 
 		for (int i = 0; i < allSkills.size(); i++) {
-			userSkillLevel = new UserSkillLevel(allSkills.get(i).getId(), allSkills.get(i).getName(), false, allSkills.get(i).getImageURL());
+			userSkillLevel = new UserSkillLevel(allSkills.get(i).getId(), allSkills.get(i).getName(), false,
+					allSkills.get(i).getImageURL());
 			for (int j = 0; j < userSkills.size(); j++) {
 				if (allSkills.get(i).getId() == userSkills.get(j).getId().getSkillId()) {
 					userSkillLevel.setChecked(true);
@@ -87,11 +88,11 @@ public class UserController {
 		}
 
 		System.out.println("==============================================================================");
-		for (int i = 0;i < userSkills.size(); i++) {
-			System.out.println("(Create) User Form: " +  userForm.getUserSkillLevels().get(i).getName());
+		for (int i = 0; i < userSkills.size(); i++) {
+			System.out.println("(Create) User Form: " + userForm.getUserSkillLevels().get(i).getName());
 			System.out.println("==============================================================================");
 		}
-		
+
 		return "user/edit";
 	}
 
@@ -137,6 +138,7 @@ public class UserController {
 	@PostMapping("/user/upsert")
 	public String postUser(UserForm userForm, @RequestParam(required = false) Long id) {
 		User user = new User();
+		System.out.println(userForm.getFirstName());
 
 		if (id != null) {
 			Optional<User> optionalUser = userRepository.findById(id);
@@ -146,68 +148,62 @@ public class UserController {
 		}
 
 		List<UserSkill> userSkills = user.getUserSkills();
-		List<UserSkillLevel> userFormList = userForm.getUserSkillLevels();
+		List<UserSkillLevel> userSkillLevels = userForm.getUserSkillLevels();
 
 		System.out.println("==============================================================================");
-		for (int i = 0;i < userSkills.size(); i++) {
+		for (int i = 0; i < userSkills.size(); i++) {
 			System.out.println("User Skills: " + userSkills.get(i).getSkill().getName());
 		}
 		System.out.println("Start ==============================================================================");
-		for (int i = 0;i < userFormList.size(); i++) {
-			System.out.println("User Form: " + userFormList.get(i).getName());
+		for (int i = 0; i < userSkillLevels.size(); i++) {
+			System.out.println("User Form: " + userSkillLevels.get(i).getName());
 		}
 		System.out.println("Stopp==============================================================================");
 
-		// Durchlaufen der UserSkillLevel-Liste
-		for (int i = 0; i < userForm.getUserSkillLevels().size(); i++) {
+		// Durchlaufen der UserSkillLevel-Liste - geht über alle skills
+		for (UserSkillLevel userSkillLevel : userSkillLevels) {
 
-			// Durchlaufen der Userskill Liste (Skills per User)
-			for (int j = 0; j < userSkills.size(); j++) {
+			if (userSkillLevel.isChecked()) {
 				Skill skill = null;
-				if (userForm.getUserSkillLevels().get(i).isChecked() == true) {
-					// add zu userSkills den Skill userForm get(i) dazu
-					if (id != null) {
-						Optional<Skill> optionalSkill = skillRepository
-								.findById(userForm.getUserSkillLevels().get(i).getId());
-						if (optionalSkill.isPresent()) {
-							skill = optionalSkill.get();
-						}
-					}
 
-					// Prüfen ob skill in der userSkill list schon dirn ist
-					// userSkills.contains(skill) gibt true zurück wenn Skill da ist --> ! Verneint,
-					// also wenn skill nicht da ist dann
-					if (!(userSkills.contains(skill))) {
-						user.addSkill(skill);
-					}
-
-					else {
-						// remove skill from userFor aus der userList
-						if (id != null) {
-							Optional<Skill> optionalSkill = skillRepository
-									.findById(userForm.getUserSkillLevels().get(i).getId());
-							if (optionalSkill.isPresent()) {
-								skill = optionalSkill.get();
-							}
-						}
-
-						// Prüfen ob der skill schon da ist
-						if (userSkills.contains(skill)) {
-							user.removeSkill(skill, userSkillRepository);
-						}
-					}
+				Optional<Skill> optionalSkill = skillRepository.findById(userSkillLevel.getId());
+				if (optionalSkill.isPresent()) {
+					skill = optionalSkill.get();
 				}
 
+				//
+
+				if (!(userSkills.contains(skill))) {
+					user.addSkill(skill);
+				}
+			}
+			else {
+				Skill skill = null;
+
+				Optional<Skill> optionalSkill = skillRepository.findById(userSkillLevel.getId());
+				if (optionalSkill.isPresent()) {
+					skill = optionalSkill.get();
+				}
+				if (userSkills.contains(skill)) {
+					user.removeSkill(skill, userSkillRepository);
+				}
 			}
 		}
 
-		userRepository.save(user);
-		
-		System.out.println("==============================================================================");
-		for (int i = 0;i < userSkills.size(); i++) {
-			System.out.println("Neue User Skills: " + user.getUserSkills().get(i).getSkill().getName());
-		}
-		System.out.println("==============================================================================");
+
+	// anzupassen
+	user.setUserName(userForm.getUserName());
+	user.setFirstName(userForm.getFirstName());
+	user.setLastName(userForm.getLastName());
+	user.setZipCode(userForm.getZipCode());
+	user.setCity(userForm.getCity());
+
+	userRepository.save(user);
+
+	System.out.println("==============================================================================");
+		for(int i = 0;i<userSkills.size();i++) {
+		System.out.println("Neue User Skills: " + user.getUserSkills().get(i).getSkill().getName());
+	}System.out.println("==============================================================================");
 
 		return "redirect:/users";
 	}
