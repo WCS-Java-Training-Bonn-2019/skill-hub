c54639d7950742cd59bda3c99d5fa42fb3df5441
Martin Langenbacher <martin.langenbacher@googlemail.com>
2020-01-30 09:58:42 +0100

    Commit for Merge: ML

--- src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.wildcodeschool.skillhub.form.UserForm;
 import com.wildcodeschool.skillhub.form.UserSkillLevel;
 import com.wildcodeschool.skillhub.model.Skill;
