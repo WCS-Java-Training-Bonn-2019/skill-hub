8bc3f0f7dbb0013998e08708c35f61303a004309
Stephanie Bauer <stephanie.bauer216@gmx.de>
2020-02-17 11:28:38 +0100

    current status

--- src/main/java/com/wildcodeschool/skillhub/config/WebMvcConfig.java
+++ src/main/java/com/wildcodeschool/skillhub/config/WebMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.wildcodeschool.skillhub.Converter.MultipartFileToByteArrayConverter;
+import com.wildcodeschool.skillhub.converter.MultipartFileToByteArrayConverter;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
