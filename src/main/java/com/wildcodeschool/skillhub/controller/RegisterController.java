13f6249218f7cabe9483e0c1f92d1b70051ee86b
Michael Berger <michael.goetz.berger@telekom.de>
2020-02-26 11:07:51 +0100

    Amend fix for user registration by admin

--- src/main/java/com/wildcodeschool/skillhub/controller/RegisterController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/RegisterController.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
